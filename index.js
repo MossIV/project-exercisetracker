cf6d1ecf3ac101e12cee7c6741710879978ba465
MossIV <waben.wong@gmail.com>
2024-10-12 15:29:36 +1300

    initialised endpoints

--- index.js
+++ index.js
@@ -9,7 +9,25 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+//create new user
+app.post("/api/users", function(req, res){
 
+})
+
+//get list of users
+app.get("/api/users", function(req, res){
+
+})
+
+//logging exercise
+app.post("/api/users/:_id/exercises", function(req, res){
+
+})
+
+//get a full log of a user
+app.get("/api/users/:_id/logs", function(req, res){
+
+})
 
 
 
